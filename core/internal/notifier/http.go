da5d2c4f037ef50a2e01e8cb84401c7df443b93e
Stéphane Cottin <stephane.cottin@vixns.com>
2018-02-20 12:16:46 +0100

    fix

--- core/internal/notifier/http.go
+++ core/internal/notifier/http.go
@@ -44,7 +44,6 @@ type HTTPNotifier struct {
 	groupWhitelist *regexp.Regexp
 	groupBlacklist *regexp.Regexp
 	extras         map[string]string
-	headers        map[string]string
 	urlOpen        string
 	urlClose       string
 	methodOpen     string
@@ -202,7 +201,7 @@ func (module *HTTPNotifier) Notify(status *protocol.ConsumerGroupStatus, eventID
 	}
 	req.Header.Set("Content-Type", "application/json")
 
-	for header, value := range module.extras {
+	for header, value := range viper.GetStringMapString("notifier." + module.name + ".headers") {
 		req.Header.Set(header, value)
 	}
 
